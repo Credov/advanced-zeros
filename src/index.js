cd29560bc764385e97dc526f686d445d5a4ac9b7
Credov <constcredov@gmail.com>
2018-10-02 18:54:32 +0300

    solved advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let i,j,q,w,e,res;
+  	res=number;
+      j=base;
+        for (i=2;i<=base;i++){
+            if (j%i===0){
+                q=0;
+                while (j%i===0){
+                      q++;
+                      j=Math.floor(j/i);
+                }
+                w=0;
+                e=number;
+                while (e/i>0){
+                      w+=Math.floor(e/i);
+                      e=Math.floor(e/i);
+                }
+                res=Math.min(res,Math.floor(w/q));
+            }
+        }
+  return res;
 }
